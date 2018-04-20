4c2afac6b520d48a938893aa55a42b7a98d95cc0
Matej Kňazík <mato.knazik@gmail.com>
2018-04-20 20:28:05 +0200

    mainwindow buttons

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,6 +1,87 @@
 #include "mainwindow.h"
 
+
 MainWindow::MainWindow()
+{
+    createButtons();
+    createMenu();
+    createCanvas();
+}
+
+void MainWindow::saveScheme()
+{
+
+}
+
+void MainWindow::deleteScheme()
+{
+
+}
+
+void MainWindow::loadScheme()
+{
+
+}
+
+void MainWindow::addBlock()
+{
+
+}
+
+void MainWindow::addLinker()
+{
+
+}
+
+void MainWindow::simulateAll()
+{
+
+}
+
+void MainWindow::simulateStep()
+{
+
+}
+
+void MainWindow::exit()
+{
+
+}
+
+void MainWindow::createMenu()
+{
+
+}
+
+void MainWindow::createButtons()
+{
+    buttonAND = new QPushButton("binary AND");
+    buttonNAND = new QPushButton("binary NAND");
+    buttonOR = new QPushButton("binary OR");
+    buttonXOR = new QPushButton("binary XOR");
+    buttonADD = new QPushButton("ADD");
+    buttonSUB = new QPushButton("SUB");
+    buttonGT = new QPushButton(">");
+    buttonLT = new QPushButton("<");
+    buttonLinker = new QPushButton("Linker");
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(buttonAND);
+    layout->addWidget(buttonNAND);
+    layout->addWidget(buttonOR);
+    layout->addWidget(buttonXOR);
+    layout->addWidget(buttonADD);
+    layout->addWidget(buttonSUB);
+    layout->addWidget(buttonGT);
+    layout->addWidget(buttonLT);
+    layout->addWidget(buttonLinker);
+
+    QWidget *central = new QWidget;
+    central->setLayout(layout);
+    setCentralWidget(central);
+}
+
+void MainWindow::createCanvas()
 {
 
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
+#include <vector>
 
 class MainWindow : public QMainWindow
 {
@@ -12,8 +18,38 @@ public:
 
 private slots:
 
+   void saveScheme();
+   void deleteScheme();
+   void loadScheme();
+   void addBlock();
+   void addLinker();
+   void simulateAll();
+   void simulateStep();
+   void exit();
+
+
 
 private:
+   void createMenu();
+   void createButtons();
+   void createCanvas();
+
+   QMenu *loadMenu;
+   std::vector<QAction*> savedSchemes;
+   QAction *save;
+   QAction *del;
+   QAction *simAll;
+   QAction *simStep;
+   QPushButton *buttonAND;
+   QPushButton *buttonOR;
+   QPushButton *buttonXOR;
+   QPushButton *buttonNAND;
+   QPushButton *buttonADD;
+   QPushButton *buttonSUB;
+   QPushButton *buttonGT;
+   QPushButton *buttonLT;
+   QPushButton *buttonLinker;
+
 
 };
 
